d9f98feb85ddcda2ef2dc2cf54d56527daa46a4f
Thiago william <thiago.will.silva@gmail.com>
2025-12-09 18:34:21 -0300

    feat: first commit

---
+++ src/components/upload-widget-upload-list.tsx
@@ -0,0 +1,26 @@
+import { useUploads } from "../store/uploads";
+import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
+
+export function UploadWidgetUploadList() {
+  const { uploads } = useUploads();
+
+  const isUploadListEmpty = uploads.size === 0;
+
+  return (
+    <div className="px-3 flex flex-col gap-3">
+      <span className="text-xs font-medium">
+        Uploaded files <span className="text-zinc-400">(2)</span>
+      </span>
+
+      {isUploadListEmpty ? (
+        <span className="text-xs text-zinc-400">No uploads added</span>
+      ) : (
+        <div className="flex flex-col gap-2">
+          {Array.from(uploads).map(([uploadId, upload]) => {
+            return <UploadWidgetUploadItem key={uploadId} upload={upload} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
