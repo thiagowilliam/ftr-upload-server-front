f6e527ff6d0a265ce45f5ad9a65b5feb0a3f081b
Thiago william <thiago.will.silva@gmail.com>
2025-12-15 19:51:41 -0300

    feat: compressão de imagens com canvas

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -4,6 +4,7 @@ import { immer } from "zustand/middleware/immer";
 import { uploadFileToStorage } from "../http/upload-file-to-storage";
 import { CanceledError } from "axios";
 import { useShallow } from "zustand/shallow";
+import { compressImage } from "../utils/compress-image";
 
 export type Upload = {
   name: string;
@@ -46,9 +47,16 @@ export const useUploads = create<UploadState, [["zustand/immer", never]]>(
         }
 
         try {
+
+          const compressedFile = await compressImage({
+            file: upload.file,
+            maxWidth: 200,
+            maxHeight: 200,
+            quality: 0.5,
+          });
           await uploadFileToStorage(
             {
-              file: upload.file,
+              file: compressedFile,
               onProgress(sizeInBytes) {
                 updateUpload(uploadId, {
                   uploadSizeInBytes: sizeInBytes,
